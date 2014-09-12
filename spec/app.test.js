a3d907f3d32bf5edd174fed2b4885d54416532d7
Dan Hough <daniel.hough@gmail.com>
2014-09-12 16:04:01 +0100

    Checking for licence.md

--- spec/app.test.js
+++ spec/app.test.js
@@ -3,6 +3,10 @@ var forkability = require('..');
 require('should');
 
 describe('forkability', function() {
+	beforeEach(function () {
+		nock.cleanAll();
+	});
+
 	it('should identify that the repo has both contributing and readme docs', function(done) {
 		nock('https://api.github.com')
 			.get('/repos/thatoneguy/thatonerepo/commits')
@@ -17,17 +21,22 @@ describe('forkability', function() {
 					path: 'contributing.md'
 				}, {
 					path: 'readme.md'
+				}, {
+					path: 'licence.md'
 				}]
 			});
 
 		forkability('thatoneguy', 'thatonerepo', function (present, missing) {
-			present.should.containEql('Contributing document').and.containEql('Readme document').and.lengthOf(2);
+			present.should.containEql('Contributing document');
+			present.should.containEql('Readme document');
+			present.should.containEql('Licence document');
+			present.should.have.a.lengthOf(3);
 			missing.should.be.empty;
 			done();
 		});
 	});
 
-	it('should identify that the repo has just a contributing doc, but no readme', function(done) {
+	it('should identify that the repo has just a contributing doc, but nothing else', function(done) {
 		nock('https://api.github.com')
 			.get('/repos/thatoneguy/thatonerepo/commits')
 			.reply(200, [{
@@ -44,7 +53,33 @@ describe('forkability', function() {
 
 		forkability('thatoneguy', 'thatonerepo', function (present, missing) {
 			present.should.containEql('Contributing document').and.lengthOf(1);
-			missing.should.containEql('Readme document').and.lengthOf(1);
+			missing.should.containEql('Readme document');
+			missing.should.containEql('Licence document');
+			missing.should.have.a.lengthOf(2);
+			done();
+		});
+	});
+
+	it('should be case insensitive about the presence of files', function(done) {
+		nock('https://api.github.com')
+			.get('/repos/thatoneguy/thatonerepo/commits')
+			.reply(200, [{
+				sha: 'fakeshalol'
+			}]);
+
+		nock('https://api.github.com')
+			.get('/repos/thatoneguy/thatonerepo/git/trees/fakeshalol')
+			.reply(200, {
+				tree: [{
+					path: 'CONTRIBUTing.md'
+				}]
+			});
+
+		forkability('thatoneguy', 'thatonerepo', function (present, missing) {
+			present.should.containEql('Contributing document').and.lengthOf(1);
+			missing.should.containEql('Readme document');
+			missing.should.containEql('Licence document');
+			missing.should.have.a.lengthOf(2);
 			done();
 		});
 	});
