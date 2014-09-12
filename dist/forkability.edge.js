6f48755ea8d0f5057d2ccf16203f1f91731135c9
Dan Hough <daniel.hough@gmail.com>
2014-09-12 22:01:26 +0100

    Handles errors... I think

--- dist/forkability.edge.js
+++ dist/forkability.edge.js
@@ -60,8 +60,18 @@
 						warnings.push({
 							message: 'Uncommented issue',
 							details: {
-								url: issue.html_url,
-								title: issue.title
+								title: issue.title,
+								url: issue.html_url
+							}
+						});
+					}
+
+					if (issue.user.login !== username && issue.labels.length === 0 && issue.comments === 0) {
+						warnings.push({
+							message: 'Untouched issue',
+							details: {
+								title: issue.title,
+								url: issue.html_url
 							}
 						});
 					}
@@ -74,70 +84,10 @@
 					},
 					warnings: warnings
 				});
+			})
+			.fail(function (err) {
+				callback(err);
 			});
-
-/*		request.get({
-			url: 'https://api.github.com/repos/' + username + '/' + repo + '/commits',
-			headers: headers,
-			json: true
-		}, function(err, response, data) {
-			var sha = data[0].sha;
-			var url = 'https://api.github.com/repos/' + username + '/' + repo + '/git/trees/' + sha;
-			// console.log(data[0]);
-			// return;
-			request.get({
-				url: url,
-				headers: headers,
-				json: true
-			}, function(err, response, data) {
-				// console.log(data);
-				var pointKeys = Object.keys(points);
-				pointKeys.forEach(function(p) {
-					var found = false;
-					var i;
-					for (i = data.tree.length - 1; i >= 0; i--) {
-						// console.log(data.tree[i])
-						if (points[p].test(data.tree[i].path)) {
-							found = true;
-							break;
-						}
-					}
-					if (found) {
-						present.push(p);
-					} else {
-						missing.push(p);
-					}
-				});
-
-				request.get({
-					url: 'https://api.github.com/repos/' + username + '/' + repo + '/issues?state=open',
-					headers: headers,
-					json: true
-				}, function(err, response, data) {
-					// console.log(data);
-
-					data.forEach(function(issue) {
-						if (issue.user.login !== username && issue.comments === 0) {
-							warnings.push({
-								message: 'Uncommented issue',
-								details: {
-									url: issue.html_url,
-									title: issue.title
-								}
-							});
-						}
-					});
-
-					callback(null, {
-						files: {
-							present: present,
-							missing: missing
-						},
-						warnings: warnings
-					});
-				});
-			});
-		});*/
 	}
 
 	module.exports = forkability;
