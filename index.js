9b0249877e764f4846b19013220ed4f3c79f6bb0
Dan Hough <daniel.hough@gmail.com>
2014-10-10 10:15:36 +0100

    Improved help docs

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ var forkability = require('./lib/app');
 var packageInfo = require('./package.json');
 var colors = require('colors');
 var repoRegex = /([^\/]*)\/([^\/]*)/;
+var languages = require('./lib/languages');
 var repoInfo;
 program
 	.version(packageInfo.version)
+	.usage('[username]/[repository]')
 	.option('-u, --username [user]', 'Username to use for auth')
 	.option('-p, --password [pass]', 'Password to use for auth')
-	.option('-l, --lang [lang,lang,lang]', 'Language(s) to lint use for lint config. Comma-separated')
-	.parse(process.argv);
+	.option('-l, --lang [lang,lang,lang]', 'Language(s) to lint use for lint config. Comma-separated');
+
+program.on('--help', function () {
+	console.log('  Supported languages for --lang:');
+	var languageKeys = Object.keys(languages);
+	for (var i = languageKeys.length - 1; i >= 0; i--) {
+		console.log('    -', languageKeys[i]);
+	}
+	console.log('\n    If you wish to contribute support for a new language, please see our CONTRIBUTING guidelines:');
+	console.log('    https://github.com/basicallydan/forkability/blob/master/CONTRIBUTING.md\n');
+	console.log('    or check for upcoming support in the issues:');
+	console.log('    https://github.com/basicallydan/forkability/issues\n');
+});
+
+program.parse(process.argv);
 
 if (!program.args[0] || !repoRegex.test(program.args[0])) {
 	throw new Error('You need to specify a repo in the format username/repository');
