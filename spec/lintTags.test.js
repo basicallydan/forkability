61655ad0491468d4cb05025ad3fb647b73903c0b
Dan Hough <daniel.hough@gmail.com>
2014-10-27 21:51:53 +0000

    Can inject tag requirements now

--- spec/lintTags.test.js
+++ spec/lintTags.test.js
@@ -4,7 +4,7 @@ describe('lintTags', function () {
 	it('should return warning of no tags found', function () {
 		var tags = [];
 
-		var report = lintTags(tags, 'joebloggs');
+		var report = lintTags(tags);
 
 		report.failures.should.containEql({
 			message: 'No tags',
@@ -20,7 +20,7 @@ describe('lintTags', function () {
 	it('should return success if a tag found', function () {
 		var tags = ['thetag'];
 
-		var report = lintTags(tags, 'joebloggs');
+		var report = lintTags(tags);
 
 		report.passes.should.containEql({
 			message: 'Tags are being used'
@@ -29,37 +29,52 @@ describe('lintTags', function () {
 		report.failures.length.should.eql(0);
 	});
 
-	// it('should return a warning for an injected tag requirement', function () {
-	// 	var tags = ['v1.1'];
+	it('should return a failure for an injected tag requirement which passes', function () {
+		var tags = ['v1.1'];
 
-	// 	var report = lintTags(tags, 'joebloggs', {
-	// 		tags: {
-	// 			'No tags called v1.0': function (tags) {
-	// 				return {
-	// 					pass: false,
-	// 					title: 'There was no tag called v1.0',
-	// 					suggestion: 'Create a tag called v1.0'
-	// 				};
-	// 			}
-	// 		}
-	// 	});
+		var report = lintTags(tags, {
+			tags: {
+				'No tags called v1.0': function (tags) {
+					return {
+						pass: false,
+						title: 'There was no tag called v1.0',
+						suggestion: 'Create a tag called v1.0'
+					};
+				}
+			}
+		});
 
-	// 	report.failures.should.containEql({
-	// 		message: 'No tags',
-	// 		details: {
-	// 			title: 'The project does not make use of git tags',
-	// 			suggestion: 'Before releasing a new version, create a tag to represent the code at the point of that release.'
-	// 		}
-	// 	});
+		report.failures.should.containEql({
+			message: 'No tags called v1.0',
+			details: {
+				title: 'There was no tag called v1.0',
+				suggestion: 'Create a tag called v1.0'
+			}
+		});
 
-	// 	report.failures.should.containEql({
-	// 		message: 'No tags',
-	// 		details: {
-	// 			title: 'The project does not make use of git tags',
-	// 			suggestion: 'Before releasing a new version, create a tag to represent the code at the point of that release.'
-	// 		}
-	// 	});
+		report.failures.length.should.eql(1);
+		report.passes.length.should.eql(1);
+	});
 
-	// 	report.failures.length.should.eql(1);
-	// });
+	it('should return a pass for an injected tag requirement which passes', function () {
+		var tags = ['v1.0'];
+
+		var report = lintTags(tags, {
+			tags: {
+				'No tags called v1.0': function (tags) {
+					return {
+						pass: true,
+						message: 'There was a tag called v1.0'
+					};
+				}
+			}
+		});
+
+		report.passes.should.containEql({
+			message: 'There was a tag called v1.0'
+		});
+
+		report.failures.length.should.eql(0);
+		report.passes.length.should.eql(2);
+	});
 });
