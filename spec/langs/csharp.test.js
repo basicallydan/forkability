d807f0f5c05003dce20071b9f327264b9fcba7a3
M-Zuber <mez613@gmail.com>
2014-12-10 16:03:19 +0200

    updated requirements for c# projects

--- spec/langs/csharp.test.js
+++ spec/langs/csharp.test.js
@@ -7,31 +7,178 @@ describe('forkability with c#', function () {
 		forkability.languages.csharp.name.should.eql('C#');
 	});
 
-	it('should identify that the repo also has recommended features for c#', function(done) {
-		mockResponses({
-			firstCommitTreeBody: {
-				tree : [{
-					path:'unit tests',
-					type: 'tree'
-				},{
-					path: 'first.csproj'
-				}]
-			}
+	describe('.csproj file', function() {
+
+		it('should return the presence of a .csproj file with a name from lowercase letters (no numbers)', function(done){
+			mockResponses({
+				firstCommitTreeBody: {
+					tree : [{
+						path: 'first.csproj'
+					},{
+						path:'source/Unit Tests',
+						type: 'tree'
+					}]
+				}
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				languages: ['csharp']
+			},
+			function (err, report) {		
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.passes.should.containEql({ message : 'Project file' });
+				report.failures.should.containEql({ message : 'Contributing document' });
+				report.failures.should.containEql({ message : 'Readme document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
 		});
 
-		forkability({
-			user: 'thatoneguy',
-			repository: 'thatonerepo',
-			languages: ['csharp']
-		},
-		function (err, report) {
-			should(err).eql(null);
-			report.passes.should.containEql({ message : 'No unit test folder' });
-			report.failures.should.containEql({ message : 'Contributing document' });
-			report.failures.should.containEql({ message : 'Readme document' });
-			report.failures.should.containEql({ message : 'Licence document' });
-			report.failures.should.containEql({ message : 'Changelog document' });
-			done();
+		it('should return the presence of a .csproj file with a name from lowercase letters and numbers', function(done){
+			mockResponses({
+				firstCommitTreeBody: {
+					tree : [{
+						path: 'first1.csproj'
+					},{
+						path:'source/Unit Tests',
+						type: 'tree'
+					}]
+				}
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				languages: ['csharp']
+			},
+			function (err, report) {		
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.passes.should.containEql({ message : 'Project file' });
+				report.failures.should.containEql({ message : 'Contributing document' });
+				report.failures.should.containEql({ message : 'Readme document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
+		});
+
+		it('should return the presence of a .csproj file with a name from uppercase letters (no numbers)', function(done){
+			mockResponses({
+				firstCommitTreeBody: {
+					tree : [{
+						path: 'FIRST.csproj'
+					},{
+						path:'source/Unit Tests',
+						type: 'tree'
+					}]
+				}
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				languages: ['csharp']
+			},
+			function (err, report) {		
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.passes.should.containEql({ message : 'Project file' });
+				report.failures.should.containEql({ message : 'Contributing document' });
+				report.failures.should.containEql({ message : 'Readme document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
+		});
+
+		it('should return the presence of a .csproj file with a name from uppercase letters and numbers', function(done){
+			mockResponses({
+				firstCommitTreeBody: {
+					tree : [{
+						path: 'F1RST.csproj'
+					},{
+						path:'source/Unit Tests',
+						type: 'tree'
+					}]
+				}
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				languages: ['csharp']
+			},
+			function (err, report) {		
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.passes.should.containEql({ message : 'Project file' });
+				report.failures.should.containEql({ message : 'Contributing document' });
+				report.failures.should.containEql({ message : 'Readme document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
+		});
+
+		it('should return the presence of a .csproj file with a name from numbers', function(done){
+			mockResponses({
+				firstCommitTreeBody: {
+					tree : [{
+						path: '1234.csproj'
+					},{
+						path:'source/Unit Tests',
+						type: 'tree'
+					}]
+				}
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				languages: ['csharp']
+			},
+			function (err, report) {		
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.passes.should.containEql({ message : 'Project file' });
+				report.failures.should.containEql({ message : 'Contributing document' });
+				report.failures.should.containEql({ message : 'Readme document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
+		});
+
+		it('should indicate the lack of a .csproj file', function(done){
+			mockResponses({
+				firstCommitTreeBody: {
+					tree : [{
+						path:'source/Unit Tests',
+						type: 'tree'
+					}]
+				}
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				languages: ['csharp']
+			},
+			function (err, report) {		
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.failures.should.containEql({ message : 'Project file' });
+				report.failures.should.containEql({ message : 'Contributing document' });
+				report.failures.should.containEql({ message : 'Readme document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
 		});
 	});
 });
