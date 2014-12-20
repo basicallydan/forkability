b0e5e6aa6575fe719b6e0ec65d53b11f800b0b61
Dan Hough <daniel.hough@gmail.com>
2014-12-20 19:36:56 +0000

    Changed nodejs to perl in the spec file

--- spec/langs/perl.test.js
+++ spec/langs/perl.test.js
@@ -2,9 +2,9 @@ var forkability = require('../..');
 var should = require('should');
 var mockResponses = require('../helper.mockResponses.js');
 
-describe('forkability with nodejs', function () {
-	it('should return the correct name for the nodejs language', function() {
-		forkability.languages.nodejs.name.should.eql('Perl');
+describe('forkability with perl', function () {
+	it('should return the correct name for the Perl language', function() {
+		forkability.languages.perl.name.should.eql('Perl');
 	});
 
 	it('should identify that the repo also has recommended features for Perl', function(done) {
