2fc1f943aa11e0dc7319a081160af5afaa763f7d
M-Zuber <mez613@gmail.com>
2015-02-04 08:36:16 +0200

    add final(?) test for depth linting

--- spec/lintDeepFiles.test.js
+++ spec/lintDeepFiles.test.js
@@ -275,4 +275,63 @@ describe('files at a depth', function() {
 			});
 		});
 	});
+
+	describe('files at all depths', function(){
+
+		it('should find the files without regard to thier depth', function (done) {
+			mockResponses({
+					firstCommitTreeBody: {
+						tree:[{
+							path: 'source',
+							type: 'tree'
+						}, {
+							path: 'source/main',
+							type: 'tree',
+							sha: 'secondSha',
+							url: 'https://api.github.com/repos/thatoneguy/thatonerepo/git/trees/secondSha'
+						}, {
+							path: 'readme'
+						}]
+					}
+			});
+
+			nock('https://api.github.com')
+			.get('/repos/thatoneguy/thatonerepo/git/trees/secondSha')
+			.reply(200, {
+				tree: [{
+					path: 'source/main/second',
+					type: 'tree',
+					sha: 'thirdSha',
+					url: 'https://api.github.com/repos/thatoneguy/thatonerepo/git/trees/thirdSha'
+				}, {
+					path: 'myProj.csproj'
+				}]
+			});
+
+			nock('https://api.github.com')
+			.get('/repos/thatoneguy/thatonerepo/git/trees/thirdSha')
+			.reply(200, {
+				tree: [{
+					path: 'source/main/second/tests',
+					type: 'tree'
+				}]
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				languages: ['csharp']
+			},
+			function(err, report){
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.passes.should.containEql({ message : 'Readme document' });
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.failures.should.containEql({ message : 'Contributing document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
+		});
+	});
 });
