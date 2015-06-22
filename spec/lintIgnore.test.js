e26712603c902050febf097eabbb141fa9481a4b
Mordechai Zuber <mez613@gmail.com>
2015-06-22 16:58:05 +0300

    Add support for per language ignore object

--- spec/lintIgnore.test.js
+++ spec/lintIgnore.test.js
@@ -87,7 +87,6 @@ describe('linting for file from .gitignore', function () {
 						type:'tree'
 					}
 				}
-				
 			},
 			function (err, report) {		
 				should(err).eql(null);
@@ -122,12 +121,125 @@ describe('linting for file from .gitignore', function () {
 					'randomExtension':/^.+\.rnd/i
 				}
 			},
-			function (err, report) {
-			console.log(err);	
+			function (err, report) {	
 				should(err).eql(null);
 				report.passes.should.containEql({ message : 'Test suite' });
 				report.failures.should.containEql({ message : 'Contributing document' });
 				report.failures.should.containEql({ message : 'randomExtension' });
+				report.failures.should.containEql({ message : 'Readme document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
+		});
+		
+		it('should pass if all the files specified in the ignore object are not in the repository', function(done){
+			mockResponses({
+				firstCommitTreeBody: {
+					tree : [{
+						path: 'first.csproj'
+					},{
+						path:'source/Unit Tests',
+						type: 'tree'
+					}]
+				}
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				ignore: {
+					'randomExtension':/^.+\.rnd/i
+				}
+			},
+			function (err, report) {
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.passes.should.containEql({ message : 'randomExtension' });
+				report.failures.should.containEql({ message : 'Contributing document' });
+				report.failures.should.containEql({ message : 'Readme document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
+		});
+		
+		it('should lint also based on per language ignore options with no options passed in', function(done){
+			var csharp = require('../lib/langs/csharp');
+			csharp.ignore = {
+					'randomExtension':/^.+\.rnd/i
+				};
+
+			mockResponses({
+				firstCommitTreeBody: {
+					tree : [{
+						path: 'first.csproj'
+					},{
+						path:'source/Unit Tests',
+						type: 'tree'
+					},{
+						path:'randomExtension.rnd'
+					}]
+				}
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				languages: ['csharp']
+			},
+			function (err, report) {
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.failures.should.containEql({ message : 'randomExtension' });
+				report.failures.should.containEql({ message : 'Contributing document' });
+				report.failures.should.containEql({ message : 'Readme document' });
+				report.failures.should.containEql({ message : 'Licence document' });
+				report.failures.should.containEql({ message : 'Changelog document' });
+				done();
+			});
+		});
+		
+		it('should lint also based on per language ignore options with options passed in', function(done){
+			var csharp = require('../lib/langs/csharp');
+			csharp.ignore = {
+					'randomExtension':/^.+\.rnd/i
+				};
+
+			mockResponses({
+				firstCommitTreeBody: {
+					tree : [{
+						path: 'first.csproj'
+					},{
+						path:'source/Unit Tests',
+						type: 'tree'
+					},{
+						path:'randomExtension.rnd'
+					},{
+						path:'debug',
+						type: 'tree'
+					}]
+				}
+			});
+
+			forkability({
+				user: 'thatoneguy',
+				repository: 'thatonerepo',
+				languages: ['csharp'],
+				ignore:{
+					'No debug folder': {
+						path: /^debug/i,
+						shouldExist: false,
+						type:'tree'
+					}
+				}
+			},
+			function (err, report) {
+				should(err).eql(null);
+				report.passes.should.containEql({ message : 'Test suite' });
+				report.failures.should.containEql({ message : 'randomExtension' });
+				report.failures.should.containEql({ message : 'No debug folder' });
+				report.failures.should.containEql({ message : 'Contributing document' });
 				report.failures.should.containEql({ message : 'Readme document' });
 				report.failures.should.containEql({ message : 'Licence document' });
 				report.failures.should.containEql({ message : 'Changelog document' });
