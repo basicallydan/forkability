0e0100391ea605c1576bebbc3bf3ad83f6917e53
Dan Hough <daniel.hough@gmail.com>
2015-08-26 21:19:05 +0900

    Improved readability of CLI output and included number of passes/failures next to header

--- index.js
+++ index.js
@@ -54,6 +54,8 @@ if (program.username && program.password) {
 function listReporter(err, report) {
 	var successSymbol = '✓';
 	var badgeTypeText = '(success)'.green;
+	var suggestionsText = '(none)'.green;
+	var featuresText = '(none)'.yellow;
 
 	if (process.platform === 'win32') {
 		successSymbol = '√';
@@ -62,14 +64,32 @@ function listReporter(err, report) {
 	if (err) {
 		return console.error('Error: ', err.message);
 	}
+
+	// Customise the output a bit
+	if (report.failures.length > 0) {
+		featuresText = ('(' + report.passes.length + ')').magenta;
+	}
+
+	if (!report.failures.length) {
+		featuresText = ('(' + report.passes.length + ')').green;
+	}
+
+	if (report.failures.length > 0) {
+		suggestionsText = ('(' + report.failures.length + ')').yellow;
+	}
+
+	if (report.badge.type !== forkability.badgeTypes.ok) {
+		badgeTypeText = '(failure)'.red;
+	}
+
 	console.log('# Forkability found'.cyan, (report.passes.length + '').magenta, 'recommended features, and has'.cyan, (report.failures.length + '').magenta, 'suggestions'.cyan);
 	console.log('');
-	console.log('# Features'.magenta);
+	console.log('# Features'.magenta, featuresText);
 	report.passes.forEach(function(pass) {
 		console.log(successSymbol.green, pass.message);
 	});
-	console.log('');
-	console.log('# Suggestions'.magenta);
+	console.log('\n---\n');
+	console.log('# Suggestions'.magenta, suggestionsText);
 	report.failures.forEach(function(failure, i) {
 		var message = failure.message;
 		if (failure.details && failure.details.suggestion) {
@@ -80,11 +100,8 @@ function listReporter(err, report) {
 			console.log(((i === report.failures.length - 1 ? '└' : '├') + '──').cyan, failure.details.title ? (failure.details.title + ':') : '', failure.details.url.cyan);
 		}
 	});
-	console.log('');
-	if (report.badge.type !== forkability.badgeTypes.ok) {
-		badgeTypeText = '(failure)'.red;
-	}
-	console.log('# Forkability Badge'.cyan, badgeTypeText);
+	console.log('\n---\n');
+	console.log('# Forkability Badge'.magenta, badgeTypeText);
 	console.log('## Just the SVG:'.cyan, '\n' + report.badge.svg);
 	console.log('## Markdown:'.cyan, '\n' + report.badge.markdown);
 	console.log('## HTML:'.cyan, '\n' + report.badge.html);
