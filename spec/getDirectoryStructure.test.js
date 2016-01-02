d4daf8458b436b0d19df2279db2bb53fae81f5ad
Dan Hough <daniel.hough@gmail.com>
2016-01-02 10:10:55 +0000

    #80 directory structure tests started...

---
+++ spec/getDirectoryStructure.test.js
@@ -0,0 +1,13 @@
+var getDirectoryStructure = require('../lib/getDirectoryStructure');
+var should = require('should');
+var path = require('path');
+
+describe('getDirectoryStructure', function() {
+	it('should return the first level of the specified directory in a github-like format', function() {
+		
+	});
+
+	it('should return the directory structure to the specified level of the specified directory in a github-like format', function() {
+		
+	});
+});
